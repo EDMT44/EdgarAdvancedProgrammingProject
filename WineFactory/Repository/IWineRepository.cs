e43eee7b780c4f9a777d533446359247c66e6450
EDMT44 <edgardiegomt@gmail.com>
2022-11-12 15:16:09 -0500

    Commenting IWineRepository and WineRepository

--- WineFactory/Repository/IWineRepository.cs
+++ WineFactory/Repository/IWineRepository.cs
@@ -13,16 +13,43 @@ namespace Repository
     /// </summary>
     public interface IWineRepository : IRepository
     {
+        /// <summary>
+        /// crea un vino en el soporte de datos
+        /// </summary>
+        /// <param name="fermenter">fermentador a usar en el vino</param>
+        /// <param name="flavor">sabor del vino</param>
+        /// <param name="id">id del vino</param>
+        /// <param name="batchId">codigo de lote</param>
+        /// <param name="state">estado del vino</param>
+        /// <param name="date">fecha en la que se hizo</param>
+        /// <returns></returns>
         Wine CreateWine(Fermenter fermenter, Flavors flavor, int id, string batchId, WineStates state, DateTime date);
-     
+        /// <summary>
+        /// Actualiza un vino en el soporte de datos
+        /// </summary>
+        /// <param name="wine"></param>
         void UpdateWine(Wine wine);
-
+        /// <summary>
+        /// Elimina un vino en el soporte de datos
+        /// </summary>
+        /// <param name="wine"></param>
         void DeleteWine(Wine wine);
-
+        /// <summary>
+        /// Obtiene un vino en el soporte de datos a partir del lote
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Wine FindWine(string id);
-    
+        /// <summary>
+        /// Obtiene un vino en el soporte de datos
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Wine GetWine(int id);
-
+        /// <summary>
+        /// Obtiene los vinos del soporte de datos
+        /// </summary>
+        /// <returns></returns>
         List<Wine> GetWines();
     }
 }
--- WineFactory/Repository/WineRepository.cs
+++ WineFactory/Repository/WineRepository.cs
@@ -9,6 +9,16 @@ namespace Repository
 {
     public partial class DBRepository : IWineRepository
     {
+        /// <summary>
+        /// Crea un vino en la base de datos
+        /// </summary>
+        /// <param name="fermenter"></param>
+        /// <param name="flavor"></param>
+        /// <param name="id"></param>
+        /// <param name="batchId"></param>
+        /// <param name="state"></param>
+        /// <param name="date"></param>
+        /// <returns></returns>
         public Wine CreateWine(Fermenter fermenter, Flavors flavor,int id, string batchId, WineStates state, DateTime date)
         {
             var wine = new Wine(fermenter, flavor, id);
@@ -18,27 +28,44 @@ namespace Repository
             Add(wine);
             return wine;
         }
-
+        /// <summary>
+        /// /Elimina un vino de la base de datos
+        /// </summary>
+        /// <param name="wine"></param>
         public void DeleteWine(Wine wine)
         {
             Delete(wine);
         }
-
+        /// <summary>
+        /// Obtiene un vino en la base de datos a partir del lote
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Wine FindWine(string id)
         {
             return Get<Wine>(w => w.BatchId == id).First();
         }
-
+        /// <summary>
+        /// Obtiene un vino en la base de datos
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Wine GetWine(int id)
         {
             return GetByID<Wine>(id);
         }
-
+        /// <summary>
+        /// Obtiene los vinos de la base de datos
+        /// </summary>
+        /// <returns></returns>
         public List<Wine> GetWines()
         {
             return Get<Wine>();
         }
-
+        /// <summary>
+        /// Actualiza un vino en la base de datos
+        /// </summary>
+        /// <param name="wine"></param>
         public void UpdateWine(Wine wine)
         {
             Update(wine);
